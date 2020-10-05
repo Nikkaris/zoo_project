b32d98182635068b3126907fa6da4188b2c93577
Nikolas Paseka <xpaseka@mendelu.cz>
2020-10-05 15:49:43 +0200

    Car with attributes

--- cv3/main.cpp
+++ cv3/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+using namespace std;
+
+class Auto{
+    float m_najetoKm;
+    float m_cenaZaDen;
+    float m_vydelanoKc;
+
+public:
+    Auto(float najetoKm, float cenaZaDen, float vydelanoKc){
+        m_najetoKm = najetoKm;
+        m_cenaZaDen = cenaZaDen;
+        m_vydelanoKc = vydelanoKc;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Auto* audi = new Auto(10000, 500, 0);
+
+    delete audi;
     return 0;
 }
