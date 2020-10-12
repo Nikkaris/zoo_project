2e62c0b68faaa4555249d9cc92a308441e7287a3
Nikolas Paseka <xpaseka@mendelu.cz>
2020-10-12 16:33:19 +0200

    PowerStation class

--- cv4/main.cpp
+++ cv4/main.cpp
@@ -16,17 +16,42 @@ public:
         return m_carMaxCurrent;
     }
 
-    void charge(float maxAh){
-        if (maxAh > (m_maxAh-m_availableAh))
+    void charge(float maxAhFromPowerStation){
+        if (maxAhFromPowerStation > (m_maxAh - m_availableAh))
             m_availableAh = m_maxAh;
         else
-            m_availableAh += maxAh;
+            m_availableAh += maxAhFromPowerStation;
+    }
+
+    void printInfo(){
+        cout << "Max: " << m_maxAh << endl;
+        cout << "Available: " << m_availableAh << endl;
+    }
+};
+
+class PowerStation{
+    float m_maxCurrent;
+    float m_hourChargeAh;
+public:
+    PowerStation(float maxCurrent, float hourChargeAh){
+        m_maxCurrent = maxCurrent;
+        m_hourChargeAh = hourChargeAh;
+    }
+
+    void chargeForHour(ElectricCar* car){
+        float charge = (car->getCarMaxCurrent()/m_maxCurrent) * m_hourChargeAh;
+        car->charge(charge);
     }
 };
 
 int main() {
-    ElectricCar* tesla = new ElectricCar(100.0, 70.0, 2.0);
+    ElectricCar* tesla = new ElectricCar(20.0, 10.0, 3.0);
+    PowerStation* ps1 = new PowerStation(4.0, 4.0);
 
+    ps1->chargeForHour(tesla);
+    tesla->printInfo();
+
+    delete ps1;
     delete tesla;
     return 0;
 }
