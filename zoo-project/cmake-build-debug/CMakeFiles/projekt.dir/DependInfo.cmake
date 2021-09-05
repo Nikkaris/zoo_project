6479dd6b2ac7c07afb8b363491c372b63c270ec0
Nikkaris <nikolas.paseka7@gmail.com>
2021-09-05 18:17:50 +0200

    github migration

---
+++ zoo-project/cmake-build-debug/CMakeFiles/projekt.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Characters/Character.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Characters/Character.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Characters/Enemy.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Characters/Enemy.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Characters/FriendlyCharacter.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Characters/FriendlyCharacter.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Characters/Hero.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Characters/Hero.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Game.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Game.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Interactions/Buy.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Interactions/Buy.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Interactions/Flirt.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Interactions/Flirt.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Interactions/Interaction.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Interactions/Interaction.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Interactions/Sell.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Interactions/Sell.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Interactions/StealCoins.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Interactions/StealCoins.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Items/Armor.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Items/Armor.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Items/Chest.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Items/Chest.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Items/Inventory.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Items/Inventory.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Items/Item.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Items/Item.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Items/Key.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Items/Key.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Items/Potion.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Items/Potion.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Items/Weapon.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Items/Weapon.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Location.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Location.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/BogLocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/BogLocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/CavernLocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/CavernLocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/ForestLocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/ForestLocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/HutLocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/HutLocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/LocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/LocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/LocationDirector.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/LocationDirector.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/NecroLocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/NecroLocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/TombLocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/TombLocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/TownLocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/TownLocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/LocationBuilders/WoodsLocationBuilder.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/LocationBuilders/WoodsLocationBuilder.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Map.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Map.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/StoryTeller.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/StoryTeller.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/BogTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/BogTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/CavernTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/CavernTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/ExitTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/ExitTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/FireTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/FireTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/ForestTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/ForestTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/HutTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/HutTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/NecroTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/NecroTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/Tile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/Tile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/TombTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/TombTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/TownTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/TownTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/Tiles/WaterTile.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/Tiles/WaterTile.cpp.obj"
+  "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/main.cpp" "C:/Users/nikol/Desktop/GIT/zoozs2020_xpaseka/projekt/cmake-build-debug/CMakeFiles/projekt.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Tiles"
+  "../LocationBuilders"
+  "../Items"
+  "../Interactions"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
